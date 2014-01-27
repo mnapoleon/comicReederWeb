9a903b2cce8961e319757b777821a2a47b2234be
Michael Napoleon <michael.napoleon@gmail.com>
2014-01-27 10:26:54 -0500

    more work to reading json

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 /** @jsx React.DOM */
 var ComicList = React.createClass({
+  getInitialState: function() {
+    return {data: []};  
+  },
+  
+  componentWillMount: function() {
+    $.ajax({
+      url: 'comics.json',
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});  
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error("comics.json", status, err.toString());
+      }.bind(this)
+    });  
+  },
+  
   render: function() {
+    console.log(this.props.data);
+    var comicRows = this.props.data.map(function (comic) {
+      return <tr><td>{comic.comic_name}</td><td>{comic.issue_number}</td><td>{comic.writer}</td><td>{comic.publisher}</td></tr>;  
+    });
     var tableContents = (
-      <tr><td>Green Lantern</td><td>1</td><td>Geoff Johns</td><td>DC Comics</td></tr>
+      <tbody>
+        {comicRows}
+      </tbody>
     );
-    return {tableContents};
+    return tableContents;
   }
 });
 
